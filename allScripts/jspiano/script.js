c6c5abee0ae989a78e91919e56aadf78c687d03d
thugperfect <meenamani9444@gmail.com>
2023-10-22 09:36:32 +0530

    all keys in array

---
+++ allScripts/jspiano/script.js
@@ -0,0 +1,80 @@
+const keys = [
+    'A0', 'Bb0', 'B0', 'C1', 'Db1', 'D1', 'Eb1', 'E1', 'F1', 'Gb1', 'G1', 'Ab1', 'A1', 'Bb1', 'B1', 'C2', 'Db2', 'D2', 'Eb2', 'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2', 'C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'Gb3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3', 'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'Gb5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5', 'C6', 'Db6', 'D6', 'Eb6', 'E6', 'F6', 'Gb6', 'G6', 'Ab6', 'A6', 'Bb6', 'B6', 'C7', 'Db7', 'D7', 'Eb7', 'E7', 'F7', 'Gb7', 'G7', 'Ab7', 'A7', 'Bb7', 'B7', 'C8'
+]
+/*
+
+B1
+C2
+C#2 or D♭2
+D2
+D#2 or E♭2
+E2
+F2
+F#2 or G♭2
+G2
+G#2 or A♭2
+A2
+A#2 or B♭2
+B2
+C3
+C#3 or D♭3
+D3
+D#3 or E♭3
+E3
+F3
+F#3 or G♭3
+G3
+G#3 or A♭3
+A3
+A#3 or B♭3
+B3
+C4 (Middle C)
+C#4 or D♭4
+D4
+D#4 or E♭4
+E4
+F4
+F#4 or G♭4
+G4
+G#4 or A♭4
+A4
+A#4 or B♭4
+B4
+C5
+C#5 or D♭5
+D5
+D#5 or E♭5
+E5
+F5
+F#5 or G♭5
+G5
+G#5 or A♭5
+A5
+A#5 or B♭5
+B5
+C6
+C#6 or D♭6
+D6
+D#6 or E♭6
+E6
+F6
+F#6 or G♭6
+G6
+G#6 or A♭6
+A6
+A#6 or B♭6
+B6
+C7
+C#7 or D♭7
+D7
+D#7 or E♭7
+E7
+F7
+F#7 or G♭7
+G7
+G#7 or A♭7
+A7
+A#7 or B♭7
+B7
+C8 (High C)
+*/
