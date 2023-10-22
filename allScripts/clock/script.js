cbc619284f62135ebaf29235f3d8ebe242c76554
thugperfect <meenamani9444@gmail.com>
2023-10-22 14:03:18 +0530

    analog clock complete

--- allScripts/clock/script.js
+++ allScripts/clock/script.js
@@ -1,43 +1,37 @@
-const min =  document.getElementById('min')
-const sec =  document.getElementById('sec')
-const startbtn = document.getElementById('start')
-const stopbtn = document.getElementById('stop')
-let secSet,minSet;
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+const startbtn = document.getElementById("start");
+const stopbtn = document.getElementById("stop");
+let secSet, minSet;
+const angle = document.getElementById("angle");
+let degreeMin = 90;
+let degreeSec = 90;
+function changeMin() {
+  degreeMin += 0.1;
+  min.style.transform = `rotate(${degreeMin}deg)`;
+  minSet = setTimeout(changeMin, 1000);
+  console.log(degreeMin);
+}
+function changeSec() {
+  degreeSec += 0.6;
+  sec.style.transform = `rotate(${degreeSec}deg)`;
+  const degree = degreeSec - degreeMin;
 
-let degreeMin = 90
-let degreeSec = 90
-function changeMin(){
-    if(degreeMin ===360){
-        degreeMin =0
-    }
-    degreeMin += 0.1
-    min.style.transform= `rotate(${degreeMin}deg)`
-    minSet = setTimeout(changeMin,1000)
-    console.log(degreeMin)
+  angle.innerText = Math.floor(degree);
+  secSet = setTimeout(changeSec, 100);
 }
-function changeSec(){
-    if(degreeSec ===360){
-        degreeSec =0
-    }
-    degreeSec += 0.6
-    sec.style.transform = `rotate(${degreeSec}deg)`
-   
-   secSet = setTimeout(changeSec,100)
+function start() {
+  changeMin();
+  changeSec();
+  startbtn.style.display = "none";
+  stopbtn.style.display = "block";
 }
-function start(){
-    changeMin()
-    changeSec()
-    startbtn.style.display ='none'
-    stopbtn.style.display = 'block'
-    
-}
-function stops(){
-    clearTimeout(secSet)
-    clearTimeout(minSet)
-  
-    startbtn.style.display ='block'
-    stopbtn.style.display = 'none'
+function stops() {
+  clearTimeout(secSet);
+  clearTimeout(minSet);
 
+  startbtn.style.display = "block";
+  stopbtn.style.display = "none";
 }
-startbtn.addEventListener('click',start)
-stopbtn.addEventListener('click',stops)
+startbtn.addEventListener("click", start);
+stopbtn.addEventListener("click", stops);
