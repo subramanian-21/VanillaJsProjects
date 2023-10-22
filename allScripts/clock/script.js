08e49824c508e788fcad4641cb50d40b1627d727
thugperfect <meenamani9444@gmail.com>
2023-10-22 13:49:06 +0530

    angle between hands

--- allScripts/clock/script.js
+++ allScripts/clock/script.js
@@ -1,5 +1,8 @@
 const min =  document.getElementById('min')
 const sec =  document.getElementById('sec')
+const startbtn = document.getElementById('start')
+const stopbtn = document.getElementById('stop')
+let secSet,minSet;
 
 let degreeMin = 90
 let degreeSec = 90
@@ -9,7 +12,7 @@ function changeMin(){
     }
     degreeMin += 0.1
     min.style.transform= `rotate(${degreeMin}deg)`
-    setTimeout(changeMin,1000)
+    minSet = setTimeout(changeMin,1000)
     console.log(degreeMin)
 }
 function changeSec(){
@@ -19,8 +22,22 @@ function changeSec(){
     degreeSec += 0.6
     sec.style.transform = `rotate(${degreeSec}deg)`
    
-    setTimeout(changeSec,100)
+   secSet = setTimeout(changeSec,100)
 }
+function start(){
+    changeMin()
+    changeSec()
+    startbtn.style.display ='none'
+    stopbtn.style.display = 'block'
+    
+}
+function stops(){
+    clearTimeout(secSet)
+    clearTimeout(minSet)
+  
+    startbtn.style.display ='block'
+    stopbtn.style.display = 'none'
 
-changeMin()
-changeSec()
+}
+startbtn.addEventListener('click',start)
+stopbtn.addEventListener('click',stops)
