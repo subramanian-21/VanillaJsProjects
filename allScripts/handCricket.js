09c90c77658b886d982b889bb3277c00bdb58818
thugperfect <meenamani9444@gmail.com>
2023-10-25 00:28:46 +0530

    not yet

--- allScripts/handCricket.js
+++ allScripts/handCricket.js
@@ -1,176 +1,208 @@
-const start_btn = document.getElementById('start')
-const game_page = document.getElementById('game')
-const right = document.getElementById('right')
-const left =  document.getElementById('left')
-const center = document.getElementById('center')
-let toss = false
-let tossWon = '';
-let tossVal = ''
+const start_btn = document.getElementById("start");
+const game_page = document.getElementById("game");
+const right = document.getElementById("right");
+const left = document.getElementById("left");
+const center = document.getElementById("center");
+let toss = false;
+let tossWon = "";
+let tossVal = "";
 let total = 0;
-let totalNa = 0
-let scoreRight = 0
-let scoreLeft = 0
-let you = ''
-const scoreDiv = document.createElement('div')
-scoreDiv.className = 'score-div'
-function start(){
-  start_btn.style.display = 'none'
-  game_page.style.display = 'block'
+let totalNa = 0;
+let scoreRight = 0;
+let scoreLeft = 0;
+let you = "";
+const leftBase = document.getElementById('left-base')
+const rightBase = document.getElementById('right-base')
+const scoreDiv = document.createElement("div");
+scoreDiv.className = "score-div";
+function start() {
+  start_btn.style.display = "none";
+  game_page.style.display = "block";
 }
 
-if(!toss){
-  const showDiv = document.createElement('div')
-  showDiv.id = 'show-div'
-  showDiv.classList.add('center')
-  const tossDiv = document.createElement('div')
-  tossDiv.className = 'center'
-  tossDiv.id = 'toss-div'
-  const tossBtn = document.createElement('button')
-  tossBtn.innerText = 'TOSS'
-  tossDiv.appendChild(tossBtn)
-  center.appendChild(showDiv)
-  center.appendChild(tossDiv)
-  tossBtn.onclick = function fun(){
-    tossBtn.style.display = 'none'
-    const odd = document.createElement('button')
-    const even = document.createElement('button')
-    odd.innerText = 'odd'
-    
-    even.innerText = 'even'
-    odd.className = 'oddeven'
-    even.className = 'oddeven'
+if (!toss) {
+  const showDiv = document.createElement("div");
+  showDiv.id = "show-div";
+  showDiv.classList.add("center");
+  const tossDiv = document.createElement("div");
+  tossDiv.className = "center";
+  tossDiv.id = "toss-div";
+  const tossBtn = document.createElement("button");
+  tossBtn.innerText = "TOSS";
+  tossDiv.appendChild(tossBtn);
+  center.appendChild(showDiv);
+  center.appendChild(tossDiv);
+  tossBtn.onclick = function fun() {
+    tossBtn.style.display = "none";
+    const odd = document.createElement("button");
+    const even = document.createElement("button");
+    odd.innerText = "odd";
 
-   
-    tossDiv.appendChild(odd)
-    tossDiv.appendChild(even)
+    even.innerText = "even";
+    odd.className = "oddeven";
+    even.className = "oddeven";
 
-    const oddeven = document.getElementsByClassName('oddeven')
-    
-    for(i = 0;i<oddeven.length;i++){
-      oddeven[i].addEventListener('click',clickfn)
+    tossDiv.appendChild(odd);
+    tossDiv.appendChild(even);
+
+    const oddeven = document.getElementsByClassName("oddeven");
+
+    for (i = 0; i < oddeven.length; i++) {
+      oddeven[i].addEventListener("click", clickfn);
     }
-  } 
+  };
 }
 
 const clickfn = function (e) {
-  
-  tossVal = e.target.innerText
-  const oddeven = document.getElementsByClassName('oddeven')
-  for(i = 0;i<oddeven.length;i++){
-    oddeven[i].style.display = 'none'
+  tossVal = e.target.innerText;
+  const oddeven = document.getElementsByClassName("oddeven");
+  for (i = 0; i < oddeven.length; i++) {
+    oddeven[i].style.display = "none";
   }
-  const showDiv = document.getElementById('show-div')
-  showDiv.innerText = tossVal
+  const showDiv = document.getElementById("show-div");
+  showDiv.innerText = tossVal;
 
-  if(tossVal){
-    const base_panel = document.getElementById('base')
-    for(i = 1;i<7;i++){
-      const click_btn = document.createElement('div')
-      click_btn.className = 'click-btn'
-      click_btn.id = i
-      click_btn.innerText = i
-      base_panel.appendChild(click_btn)
-    }    
-    const all_btns = document.querySelectorAll('.click-btn')
+  if (tossVal) {
+    const base_panel = document.getElementById("base");
+    for (i = 1; i < 7; i++) {
+      const click_btn = document.createElement("div");
+      click_btn.className = "click-btn";
+      click_btn.id = i;
+      click_btn.innerText = i;
+      base_panel.appendChild(click_btn);
+    }
+    const all_btns = document.querySelectorAll(".click-btn");
 
-    for(i = 0;i<all_btns.length;i++){
-      all_btns[i].addEventListener('click',clickFn)
+    for (i = 0; i < all_btns.length; i++) {
+      all_btns[i].addEventListener("click", clickFn);
     }
   }
-}
-const clickFn =(e)=>{
-    const random = Math.floor(1+Math.random()*6)
-    const click = Number(e.target.id)
-    right.innerText = click
-    scoreRight += click
+};
+const clickFn = (e) => {
+  if (!toss) {
+    const random = Math.floor(1 + Math.random() * 6);
+    const click = Number(e.target.id);
+    right.innerText = click;
+    scoreRight += click;
+
+    left.innerText = random;
+    total = random + click;
+    if (total % 2 === 0) {
+      totalNa = "even";
+    } else {
+      totalNa = "odd";
+    }
+    if (tossVal === totalNa) {
+      tossWon = "won";
+    } else {
+      tossWon = "loose";
+    }
+  }
+
+  tossStart();
+};
+function mainStart() {
+  const mStart = document.getElementById("start-game");
+  center.appendChild(scoreDiv);
+
+  mStart.addEventListener("click", () => {
+    const baseWrap = document.getElementById("base-wrap");
+    baseWrap.style.display = "block";
+    mStart.style.display ="none"
+    const all_btns = document.querySelectorAll(".click-btn");
+    scoreRight = 0;
+    const showDiv = document.getElementById("show-div");
    
-    left.innerText = random 
-    
-    total = random+click
-    if(total%2 === 0){
-      totalNa = 'even'
-    }else{
-      totalNa = 'odd'
-    }
-    if(tossVal === totalNa){
-      tossWon ='won'
-    }else{
-      tossWon = 'loose'
-    }
-
-   tossStart()
-
-  }
-function mainStart(){
-    const mStart = document.getElementById('start-game')
-    center.appendChild(scoreDiv)
-    mStart.addEventListener('click',()=>{
-        const baseWrap = document.getElementById('base-wrap')
-        baseWrap.style.display = 'block'
-        if(you === 'batting'){
-            const all_btns = document.querySelectorAll('.click-btn')
-            all_btns.forEach(k=>{
-                k.addEventListener('click',()=>{
-                    
-                })
-            })
-        }else{
-    
-        }
-    })
-}
-function tossStart(){  
-    if(!toss){
-        const baseWrap = document.getElementById('base-wrap')
-        baseWrap.style.display = 'none'
-        const showDiv = document.getElementById('show-div')
-        showDiv.innerText = `Toss ${tossWon}`
-        toss = true
-        if(tossWon === 'won'){
-            const wrap_select = document.createElement('div')
-    
-            const batting = document.createElement('button')
-            const bowling = document.createElement('button')
-            batting.innerText ='batting'
-            batting.className = 'select-toss'
-            bowling.innerText = 'bowling'
-            bowling.className = 'select-toss'
-            showDiv.appendChild(wrap_select)
-           wrap_select.appendChild(batting)
-           wrap_select.appendChild(bowling)
-          const selectToss = document.getElementsByClassName('select-toss')
+    for (i = 0; i < all_btns.length; i++) {
+      all_btns[i].addEventListener("click", (e) => {
+        showDiv.innerText = you
+        if (you === "batting") {
+            
+          const random = Math.floor(1 + Math.random() * 6);
+          const click = Number(e.target.id);
+          right.innerText = click;
           
-          for(i =0;i<selectToss.length;i++){
-            selectToss[i].addEventListener('click',(e)=>{
-                you = e.target.innerText
-                showDiv.innerText=you
-                const mStart = document.createElement('button')
-                mStart.innerText = "start game"
-                mStart.id = 'start-game'
-                showDiv.appendChild(mStart)
-                mainStart()
-            })
-          }
+          left.innerText = random;
+          if(random!==click){
+            scoreRight += click;
+            scoreDiv.innerText = scoreRight
           }else{
-            const computerChooseRandom = Math.floor(Math.random()*2)
-            console.log(computerChooseRandom)
-            const batOrBowl = ['batting','bowling']
-            const notifyPlayer = document.createElement('div')
-            notifyPlayer.className = 'white'
-            if(batOrBowl[computerChooseRandom] === 'batting'){
-                notifyPlayer.innerText = 'You Bat'
-                you = 'batting'
-            }else{
-                notifyPlayer.innerText = 'You Bowl'
-                you = 'bowling'
-            }
-            const mStart = document.createElement('button')
-            mStart.innerText = "start game"
-            mStart.id = 'start-game'
-            showDiv.appendChild(mStart)
-            showDiv.appendChild(notifyPlayer)
-            mainStart()
-          }  
-    }  
-    
+            scoreDiv.innerText = 'out'
+            you = 'bowling'
+            right.innerText = ''
+            left.innerText = ''
+            rightBase.innerText = `your score : ${scoreRight}`
+          }
+          
+          
+        } else {
+            const random = Math.floor(1 + Math.random() * 6);
+            const click = Number(e.target.id);
+            right.innerText = click;
+            left.innerText = random;
+            if(random!==click){
+                scoreLeft += random;
+                scoreDiv.innerText = scoreLeft
+              }else{
+                scoreDiv.innerText = 'out'
+                console.log('shit')
+              }
+        }
+      });
+    }
+  });
+}
+function tossStart() {
+  if (!toss) {
+    const baseWrap = document.getElementById("base-wrap");
+    baseWrap.style.display = "none";
+    const showDiv = document.getElementById("show-div");
+    showDiv.innerText = `Toss ${tossWon}`;
+    toss = true;
+    if (tossWon === "won") {
+      const wrap_select = document.createElement("div");
+
+      const batting = document.createElement("button");
+      const bowling = document.createElement("button");
+      batting.innerText = "batting";
+      batting.className = "select-toss";
+      bowling.innerText = "bowling";
+      bowling.className = "select-toss";
+      showDiv.appendChild(wrap_select);
+      wrap_select.appendChild(batting);
+      wrap_select.appendChild(bowling);
+      const selectToss = document.getElementsByClassName("select-toss");
+
+      for (i = 0; i < selectToss.length; i++) {
+        selectToss[i].addEventListener("click", (e) => {
+          you = e.target.innerText;
+          showDiv.innerText = you;
+          const mStart = document.createElement("button");
+          mStart.innerText = "start game";
+          mStart.id = "start-game";
+          showDiv.appendChild(mStart);
+          mainStart();
+        });
+      }
+    } else {
+      const computerChooseRandom = Math.floor(Math.random() * 2);
+      const batOrBowl = ["batting", "bowling"];
+      const notifyPlayer = document.createElement("div");
+      notifyPlayer.className = "white";
+      if (batOrBowl[computerChooseRandom] === "batting") {
+        notifyPlayer.innerText = "You Bat";
+        you = "batting";
+      } else {
+        notifyPlayer.innerText = "You Bowl";
+        you = "bowling";
+      }
+     
+      const mStart = document.createElement("button");
+      mStart.innerText = "start game";
+      mStart.id = "start-game";
+      showDiv.appendChild(mStart);
+      showDiv.appendChild(notifyPlayer);
+      mainStart();
+    }
+  }
 }
